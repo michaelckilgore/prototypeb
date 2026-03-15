b8655d47e543818012503544b95db32820d66a16
michaelckilgore <michaelckilgore@gmail.com>
2026-03-15 13:41:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1688,6 +1688,7 @@ function getNextScreenHref() {
   if (path.endsWith("subscreen-current.html")) return "subscreen-regional-map.html";
   if (path.endsWith("subscreen-regional-map.html")) return "subscreen-watch-sample.html";
   if (path.endsWith("subscreen-watch-sample.html")) return "subscreen-forecast.html";
+  if (path.endsWith("subscreen-watch.html")) return "subscreen-forecast.html";
   if (path.endsWith("subscreen-forecast.html")) return "index.html";
   return "subscreen-current.html";
 }
