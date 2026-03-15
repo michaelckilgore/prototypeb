55e7d90da1863fd565d9eee9cc69aa7b0fc9b9ac
michaelckilgore <michaelckilgore@gmail.com>
2026-03-15 13:01:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1685,9 +1685,10 @@ function getNextScreenHref() {
   const path = window.location.pathname.toLowerCase();
 
   if (path.endsWith("index.html") || path.endsWith("/prototypeb-main/") || path.endsWith("/")) return "subscreen-current.html";
-  if (path.endsWith("subscreen-current.html")) return "subscreen-forecast.html";
-  if (path.endsWith("subscreen-forecast.html")) return "subscreen-regional-map.html";
-  if (path.endsWith("subscreen-regional-map.html")) return "index.html";
+  if (path.endsWith("subscreen-current.html")) return "subscreen-regional-map.html";
+  if (path.endsWith("subscreen-regional-map.html")) return "subscreen-watch-sample.html";
+  if (path.endsWith("subscreen-watch-sample.html")) return "subscreen-forecast.html";
+  if (path.endsWith("subscreen-forecast.html")) return "index.html";
   return "subscreen-current.html";
 }
 
